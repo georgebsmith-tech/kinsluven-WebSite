6b2315ee6d33af081665f86987115290bcdb3d03
George, B. Smith <georgebsmith.tech@gmail.com>
2020-08-17 12:30:57 -0700

    added files

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const app = express()
 
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set("views", "views")
+app.set("view engine", "ejs")
 app.get("/", (req, res) => {
-    res.end("This is it")
+    res.render("index", { title: "Home of furnitures" })
 })
 
 
